9e6a192af7493d91f8480448f4621ecc81a735df
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-12-19 01:08:08 +0100

    Changed to "What *do* you see?"

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 
 mkdir test
 mkdir test/landscape
-java -Xms4096M -Xmx4096M -jar openstego-0.6.1/lib/openstego.jar extract -a RandomLSB -sf Decoration_du_sapin_de_Noel_landscape_wcys.png -xd test/landscape
+java -Xms4096M -Xmx4096M -jar openstego-0.6.1/lib/openstego.jar extract -a RandomLSB -sf Decoration_du_sapin_de_Noel_landscape_wdys.png -xd test/landscape
 cd test/landscape
 uudecode -c uuencoded-source-data.asc
 cd ../..
 mkdir test/portrait
-java -Xms4096M -Xmx4096M -jar openstego-0.6.1/lib/openstego.jar extract -a RandomLSB -sf Decoration_du_sapin_de_Noel_portrait_wcys.png -xd test/portrait
+java -Xms4096M -Xmx4096M -jar openstego-0.6.1/lib/openstego.jar extract -a RandomLSB -sf Decoration_du_sapin_de_Noel_portrait_wdys.png -xd test/portrait
 cd test/portrait
 uudecode -c uuencoded-source-data.asc
